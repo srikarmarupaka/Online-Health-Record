bef39ec6c30454dde9b81577399854f224546aa8
srikarmarupaka <srikarsharmamarpaka@gmail.vom>
2021-04-09 15:17:32 +0530

    carousel adjusted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@
 import { Container } from 'react-bootstrap';
 import Signup from './components/LoginSignup';
 import Dashboard from './components/Dashboard';
+import Admin from './components/Admin';
+import Hospital from './components/Hospital';
+import College from './components/College';
 import UpdateProfile from './components/UpdateProfile';
 import Login from './components/Login';
 import ForgotPassword from './components/ForgotPassword'
@@ -23,6 +26,9 @@ function App() {
           <Router>
           <AuthProvider>
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/dashboard/admin" component={Admin} />
+            <PrivateRoute exact path="/dashboard/Hospital" component={Hospital} />
+            <PrivateRoute exact path="/dashboard/College" component={College} />
             <PrivateRoute exact path="/update-profile" component={UpdateProfile} />
             <Route exact path="/" component={LandingPage} />
             <Route path="/signup" component={Signup} />
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,23 +3,35 @@ import { Carousel } from 'react-bootstrap'
 
 export default function Carosel() {
     return (
-        <div>
-            <Carousel>
+        <>
+            <Carousel style={{width:"100vh"}}>
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
-                    src="./images/cover1.jpeg"
+                    src="./images/EHR1.jpg"
                     alt="First slide"
                   />
                   <Carousel.Caption>
-                    <h3>First slide label</h3>
+                    <h3>Digital Health Record</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
-                    src="./images/digitab.jpeg"
+                    src="./images/EHR2.jpg"
+                    alt="Second slide"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="./images/EHR3.jpeg"
                     alt="Second slide"
                   />
 
@@ -41,6 +53,6 @@ export default function Carosel() {
                   </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-        </div>
+        </>
     )
 }
